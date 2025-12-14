9a2f836649a73bb7d8a158cbc4361bf0e6f7c8ac
Vince Au <vinceau09@gmail.com>
2025-12-15 10:04:02 +1100

    feat: add ability to fetch all stages by game mode (#155)
    * make stages an object, not string
    
    * add mode to stages
    
    * add ability to fetch all the stages of a particular game mode

--- src/common/melee/stageUtils.ts
+++ src/common/melee/stageUtils.ts
@@ -1,29 +1,48 @@
-import stageNames from "./stages.json";
+import stagesInfoMap from "./stages.json";
+
+type StageMode = "vs" | "target-test" | "home-run-contest";
 
 export type StageInfo = {
   id: number;
   name: string;
+  mode?: StageMode;
 };
+
+const ALL_STAGES = new Map<number, StageInfo>(
+  Object.entries(stagesInfoMap).map(([key, value]) => {
+    const stageId = parseInt(key);
+    const info: StageInfo = {
+      id: stageId,
+      name: value.name,
+      mode: value.mode as StageMode,
+    };
+    return [stageId, info];
+  }),
+);
 
 export const UnknownStage: StageInfo = {
   id: -1,
   name: "Unknown Stage",
 };
 
-type StageId = keyof typeof stageNames;
-
 export function getStageInfo(stageId: number): StageInfo {
-  const stageName = stageNames[stageId.toString() as StageId];
-  if (!stageName) {
+  const stageInfo = ALL_STAGES.get(stageId);
+  if (!stageInfo) {
     return UnknownStage;
   }
   return {
     id: stageId,
-    name: stageName,
+    name: stageInfo.name,
   };
 }
 
 export function getStageName(stageId: number): string {
   const stage = getStageInfo(stageId);
   return stage.name;
+}
+
+export function getStages(mode: "all" | StageMode = "vs"): StageInfo[] {
+  return Array.from(ALL_STAGES.values()).filter((stage) => {
+    return mode === "all" || stage.mode === mode;
+  });
 }
